e68637eda8c024a3009a295cc23b84d191f3e460
lemin2601 <lemin2601@gmail.com>
2017-05-10 16:29:02 +0700

    Initial commit

---
+++ app/src/main/java/com/leemin/minhtrung/testdownloadimage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.leemin.minhtrung.testdownloadimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
